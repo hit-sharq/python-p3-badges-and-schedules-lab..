be50661ef2fa4f369ebfcf128ae6e3350b73684a
joshua mwendwa <leeroyjoshu087@gmail.com>
2024-09-02 19:33:50 +0300

    added

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,24 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    return [f"Hello, my name is {name}." for name in names]
 
-def assign_rooms(names):
-    return None
+def assign_rooms(speakers):
+    room_assignments = []
+    for index, speaker in enumerate(speakers, start=1):
+        room_assignments.append(f"Hello, {speaker}! You'll be assigned to room {index}!")
+    return room_assignments
 
-def printer(names):
-    return None
+def printer(speakers):
+    badges = batch_badge_creator(speakers)
+    room_assignments = assign_rooms(speakers)
+    
+    for badge in badges:
+        print(badge)
+    
+    for assignment in room_assignments:
+        print(assignment)
+
+speaker_list = ["Arel", "Carol", "John"]
+printer(speaker_list)
